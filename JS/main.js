abb60eda2eded6f33d4f060ac4502b2a2a2072e5
italo-rodrigues1 <italosport110@hotmal.com>
2021-03-23 20:23:45 -0300

    erro menuMobile corrigido

--- JS/main.js
+++ JS/main.js
@@ -76,17 +76,20 @@ function menuMobile() {
   const menu = document.getElementById('menu-hamburger');
   const menuOpc = document.querySelector('.menu-mobile ul');
   const menuClose = document.getElementById('menu-close-hamburger');
+  const menuContainer = document.querySelector('.menu-mobile');
 
   menu.addEventListener('click',()=>{
     menuOpc.style.right = '0';
     menuClose.style.display = 'flex';
     menu.style.display = 'none';
+    menuContainer.style.visibility = 'visible';
   })
   
   menuClose.addEventListener('click',()=>{
     menuOpc.style.right = '-100%';
     menu.style.display = 'flex';
     menuClose.style.display = 'none';
+    menuContainer.style.visibility = 'hidden';
   })
 
 }
